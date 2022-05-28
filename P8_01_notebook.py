62a0072d927620cc219d79d5a4409b1b3aaddfb0
lancelot <lancelot.leclercq@free.fr>
2022-05-28 03:03:09 +0200

    amélioration bovw spark

--- P8_01_notebook.py
+++ P8_01_notebook.py
@@ -13,8 +13,7 @@ from pyspark.ml.clustering import KMeans
 import cv2 as cv
 
 # %%
-spark = SparkSession.builder.appName('FruitsPreProc').config(
-    "spark.driver.memory", "8g").getOrCreate()
+spark = SparkSession.builder.appName('FruitsPreProc').getOrCreate()
 sc = spark.sparkContext
 spark.sparkContext._conf.getAll()
 
@@ -35,7 +34,7 @@ def load_img_data(path='./fruits-360_dataset/fruits-360/Training/'):
         F.concat('label', F.lit('_'),
                  F.element_at(F.split(F.col('path'), '/'), -1)))
     ImgData = ImgData.drop('path')
-    return ImgData.sample(.1)
+    return ImgData.sample(.01)
 
 
 # %%
@@ -45,21 +44,12 @@ ImgData = load_img_data()
 # %%
 def get_desc(img):
     image = cv.imread(img)
-    if image is not None:
-        imgR = cv.resize(image, (224, 224), interpolation=cv.INTER_AREA)
-        imgBW = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
-        imgBWCLAHE = cv.createCLAHE(clipLimit=8,
-                                    tileGridSize=(3, 3)).apply(imgBW)
-        imgBWCLAHENlMD = cv.fastNlMeansDenoising(imgBWCLAHE, None, 5, 7, 21)
-        orb = cv.ORB_create(nfeatures=100)
-        keypoints_orb, desc = orb.detectAndCompute(imgBWCLAHENlMD, None)
-        if desc is None:
-            desc = [np.array(32 * [0]).astype(np.float64).tolist()]
-        else:
-            desc = desc.astype(np.float64).tolist()
-    else:
+    orb = cv.ORB_create(nfeatures=100)
+    keypoints_orb, desc = orb.detectAndCompute(image, None)
+    if desc is None:
         desc = [np.array(32 * [0]).astype(np.float64).tolist()]
-
+    else:
+        desc = desc.astype(np.float64).tolist()
     return desc
 
 
@@ -71,17 +61,18 @@ udf_image = F.udf(
 ImgDesc = ImgData.drop('content').withColumn("descriptors",
                                              F.explode(udf_image("TruePath")))
 
-ImgDesc = ImgDesc.filter(ImgDesc.descriptors.isNotNull())
-
 # %%
-model = KMeans(k=1000,
-               featuresCol='descriptors').fit(ImgDesc)
+kmean = KMeans(k=1000, featuresCol='descriptors', seed=0)
+model = kmean.fit(ImgDesc)
+
 # %%
-ImgPred = model.transform(ImgDesc)
-ImgPred.show(3)
+Pred = model.transform(ImgDesc)
+Pred.show(3)
 # %%
-ImgPred = ImgPred.groupBy('label', 'prediction').count().fillna(0)
+ImgPred = Pred.groupBy('label', 'prediction').count()
 #%%
-BoVW = ImgPred.groupBy('label').pivot('prediction').show(3)
+BoVW = ImgPred.groupBy('label').pivot('prediction').sum('count').fillna(
+    0)
+BoVW.show()
 
 # %%
